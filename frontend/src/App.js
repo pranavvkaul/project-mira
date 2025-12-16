8a42673e2c9f4f000c6c562bb94fcc3a1bcee1d7
Pranav Kaul <pranavkaul0904@gmail.com>
2025-12-16 16:39:01 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   return (
+    // 'App' class is a standard convention, often used for global styling.
     <div className="App">
       <PropertyComparison />
     </div>
